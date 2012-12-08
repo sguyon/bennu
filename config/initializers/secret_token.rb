b6c28ba60f5b1e91f827c92a265b6c523bf98714
Sacha Guyon <sacha.guyon@gmail.com>
2012-12-08 16:27:56 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bennu::Application.config.secret_token = '4304f682f10981e50e1fe0f1cd0e1eefa730945740578d59cb74935cf00e9b4f83d918229467102b0c766e9794c9d7bb9b295c73556fc68d90c40c09667a648b'
