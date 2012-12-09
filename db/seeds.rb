90fba79e9964f4a1281d483a87d3e8b5d6d56204
Sacha Guyon <sacha.guyon@gmail.com>
2012-12-09 12:41:59 +0800

    before scaffold sticker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+##### ROLES
 puts 'CREATING ROLES'
+Role.destroy_all
 Role.create([
   { :name => 'admin' }, 
-  { :name => 'user' }, 
-  { :name => 'VIP' }
+  { :name => 'user' }
 ], :without_protection => true)
+
+##### USERS
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+User.destroy_all
+user = User.create! :name => 'Bennu Admin', :email => 'sacha.guyon@gmail.com', :password => 'admin1', :password_confirmation => 'admin1'
 user.confirm!
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'Manufacturer 1', :email => 'manufacturer@email.com', :password => 'manufacturer', :password_confirmation => 'manufacturer'
 user2.confirm!
 puts 'New user created: ' << user2.name
 user.add_role :admin
-user2.add_role :VIP
+user2.add_role :user
+
+##### MESSAGES
+Message.create([
+  {
+    :name => "french product 1",
+    :error => "Ce n'est pas le bon produit",
+    :confirm => "Verification OK, c'est bien un produit 1"
+  },
+  {
+    :name => "french product 2",
+    :error => "Ce n'est pas le bon produit",
+    :confirm => "Verification OK, c'est bien un produit 2"
+  },
+  {
+    :name => "english product 1",
+    :error => "This is not the good product",
+    :confirm => "Verification OK, it is a product 1"
+  }
+])
+
+
+##### STICKERS
+
+
+
+
+
+
